61154fa44c041d4b45edeaf69f140c687d97ca95
samokasha <okasha86@hotmail.com>
2019-01-07 16:50:56 +0400

    Add workers

--- index.js
+++ index.js
@@ -1,104 +1,20 @@
 // Primary file for the API
 
-const http = require("http");
-const https = require("https");
-const url = require("url");
-const StringDecoder = require("string_decoder").StringDecoder;
-const config = require("./config/config");
-const fs = require("fs");
-const handlers = require("./lib/handlers");
-const helpers = require("./lib/helpers");
+const server = require("./lib/server");
+const workers = require("./lib/workers");
 
-// @TODO remove this
-helpers.sendTwilioSms("6472242454", "Hello", err => {
-  console.log(err);
-});
+// Declare app
+const app = {};
 
-// Instatiate the http server
-const httpServer = http.createServer((req, res) => {
-  unifiedServer(req, res);
-});
-
-// Start the server
-httpServer.listen(config.httpPort, () => {
-  console.log(`The server is listening on port ${config.httpPort}`);
-});
-
-// Instatiate https server
-const httpsServerOptions = {
-  key: fs.readFileSync("./https/key.pem"),
-  cert: fs.readFileSync("./https/cert.pem")
-};
-const httpsServer = https.createServer(httpsServerOptions, (req, res) => {
-  unifiedServer(req, res);
-});
-
-// Start the https server
-httpsServer.listen(config.httpsPort, () => {
-  console.log(`The server is listening on port ${config.httpsPort}`);
-});
-
-// All the server logic for both the http and https server
-const unifiedServer = (req, res) => {
-  // Get url and parse it
-  const parsedUrl = url.parse(req.url, true); // 2nd arg tell url.parse to forward query to query lib
-
-  // Get path from url
-  const path = parsedUrl.pathname;
-  const trimmedPath = path.replace(/^\/+|\/+$/g, ""); // removes leading slash
-
-  // Get the query string as object
-  const queryStringObject = parsedUrl.query;
-
-  // Get HTTP method
-  const method = req.method.toLowerCase();
-
-  // Get headers as object
-  const headers = req.headers;
-
-  // Get payload, if any
-  var decoder = new StringDecoder("utf-8");
-  let buffer = "";
-  req.on("data", data => {
-    buffer += decoder.write(data);
-  });
-  req.on("end", () => {
-    buffer += decoder.end();
-
-    // Choose handler this req should go to. If none found ,use notFound handler
-    const chosenHandler = router[trimmedPath]
-      ? router[trimmedPath]
-      : handlers.notFound;
-
-    // construct data object to send to handler
-    const data = {
-      trimmedPath,
-      queryStringObject,
-      method,
-      headers,
-      payload: helpers.parseJsonToObject(buffer)
-    };
-
-    // Route the request to the handler specified in the router
-    chosenHandler(data, (statusCode = 200, payload = {}) => {
-      // Convert payload to string
-      const payloadString = JSON.stringify(payload);
-      console.log("payloadString ", payloadString);
-
-      // Return response
-      res.setHeader("Content-Type", "application/json");
-      res.writeHead(statusCode);
-      res.end(payloadString);
-      // Log req path
-      console.log("Returning response: ", statusCode, payloadString);
-    });
-  });
+// Init fn
+app.init = () => {
+  // start the server
+  server.init();
+  // start the workers
+  workers.init();
 };
 
-// Define a request router
-const router = {
-  ping: handlers.ping,
-  users: handlers.users,
-  tokens: handlers.tokens,
-  checks: handlers.checks
-};
+// exec
+app.init();
+
+module.exports = app;
